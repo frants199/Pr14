218a2f23b74360845bef977cfb9951626f7c9e7d
Natalya <65410808+frants199@users.noreply.github.com>
2020-06-04 21:17:54 +0300

    Добавьте файлы проекта.

---
+++ PR14_WF/ReportForm.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows.Forms;
+using Microsoft.Reporting.WinForms;
+
+namespace PR14_WF
+{
+	public partial class ReportForm : Form
+	{
+		public ReportForm()
+		{
+			InitializeComponent();
+		}
+
+		private void ReportForm_Load(object sender, EventArgs e)
+		{
+			reportViewer1.ProcessingMode = ProcessingMode.Local;
+			reportViewer1.LocalReport.ReportPath = ("Report.rdlc");
+			ReportDataSource datasource = new ReportDataSource("StudentsDataSet", this.studentsDataSet1.Tables["students"]);
+			reportViewer1.LocalReport.DataSources.Clear();
+			reportViewer1.LocalReport.DataSources.Add(datasource);
+			this.reportViewer1.RefreshReport();
+		}
+	}
+}
